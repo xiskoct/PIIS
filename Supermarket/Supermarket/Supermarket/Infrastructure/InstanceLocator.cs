97f9788373178dbc1fe8690570fdfe577c11e24b
Franico <Franico@DESKTOP-AKD1KF9>
2017-04-11 10:37:12 +0200

    Creando Jerarquía de entorno de trabajo

---
+++ Supermarket/Supermarket/Supermarket/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+
+namespace Supermarket.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public class InstanceLocator
+        {
+            public InstanceLocator()
+            {
+                Main = new MainViewModel();
+            }
+            public MainViewModel Main { get; set; }
+        }
+}
